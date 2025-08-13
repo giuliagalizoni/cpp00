e6701b31a3cc6f8518055244ae1617fcee1f21cd
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-08-13 17:05:10 +0200

    solution to ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return 0;
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		std::string s = argv[i];
+		std::transform(s.begin(), s.end(), s.begin(), (int(*)(int))std::toupper);
+		std::cout << s;
+	}
+	std::cout << "\n";
+
+	return 0;
+}
