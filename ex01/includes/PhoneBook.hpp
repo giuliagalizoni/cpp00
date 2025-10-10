3a5e6b73cb616ac21816d51f78354a8628db511e
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-10-10 13:55:05 +0200

    cleanup and organize

--- ex01/includes/PhoneBook.hpp
+++ ex01/includes/PhoneBook.hpp
@@ -1,29 +1,30 @@
 #ifndef PHONEBOOK_CLASS
-# define PHONEBOOK_CLASS
+#define PHONEBOOK_CLASS
 
-# include <string>
-# include "Contact.hpp"
+#include <string>
+#include "Contact.hpp"
 
 class PhoneBook
 {
 private:
-	Contact	_contacts[8];
-	int	_count;
+	Contact _contacts[8];
+	int _count;
 
-	Contact	_collectContactData(int index);
-	Contact	_getContact(int index) const;
-	bool	_fillField(std::string& field, std::string field_name);
-	bool	_validateString(std::string field);
-	bool	_validateNumber(std::string field);
-	void	_displayContactTable(int contacts_to_show);
-	void	_displayContact(Contact contact);
-	std::string	_formatString(std::string text);
+	Contact _collectContactData(int index);
+	Contact _getContact(int index) const;
+	bool _fillField(std::string &field, std::string field_name);
+	bool _validateString(std::string field);
+	bool _validateNumber(std::string field);
+	void _displayContactTable(int contacts_to_show);
+	void _displayContact(Contact contact);
+	std::string _formatString(std::string text);
+
 public:
 	PhoneBook();
 	~PhoneBook();
 
-	void	addContact();
-	void	search();
+	void addContact();
+	void search();
 };
 
 #endif
--- ex01/src/PhoneBook.cpp
+++ ex01/src/PhoneBook.cpp
@@ -5,15 +5,30 @@
 
 PhoneBook::PhoneBook() : _count(0)
 {
-	// std::cout << "PhoneBook constructor called" << std::endl;
-	// std::cout << "PhoneBook initialized with " << _count << " contacts" << std::endl;
 	return;
 }
 
 PhoneBook::~PhoneBook()
 {
-	// std::cout << "PhoneBook destructor called" << std::endl;
 	return;
+}
+
+void PhoneBook::addContact()
+{
+
+	int index = _count % 8;
+	Contact contact = _collectContactData(index);
+	if (contact.get_index() == -1)
+		return;
+	if (_count >= 8)
+	{
+		std::cout << "Replacing contact at position " << index << std::endl;
+	}
+
+	this->_contacts[index] = contact;
+	_count++;
+	std::cout << "Contact " << contact.get_first_name() << " was created"
+			  << std::endl;
 }
 
 void PhoneBook::search()
@@ -44,87 +59,41 @@ void PhoneBook::search()
 	int contact_index = std::atoi(input.c_str());
 	while (contact_index < 0 || contact_index >= contacts_to_show)
 	{
-		std::cout << "Invalid index. Please enter a number between 0 and " << (contacts_to_show - 1) << std::endl;
+		std::cout << "Invalid index. Please enter a number between 0 and "
+				  << (contacts_to_show - 1) << std::endl;
 		if (!std::getline(std::cin, input))
 			return;
 		contact_index = std::atoi(input.c_str());
 	}
 	_displayContact(_getContact(contact_index));
 }
-void PhoneBook::_displayContactTable(int contacts_to_show)
-{
-	std::cout << "Which contact would you like to see?" << std::endl;
-	std::cout << std::setw(10) << "Index" << "|"
-			  << std::setw(10) << "First Name" << "|"
-			  << std::setw(10) << "Last Name" << "|"
-			  << std::setw(10) << "Nickname" << std::endl;
-	std::cout << "__________|__________|__________|__________" << std::endl;
 
-	for (int i = 0; i < contacts_to_show; i++)
+Contact PhoneBook::_collectContactData(int index)
+{
+	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+	std::string phone;
+	std::string secret;
+
+	std::cout << "=== ADD NEW CONTACT ===" << std::endl;
+	if (!_fillField(first_name, "first name") ||
+		!_fillField(last_name, "last name") ||
+		!_fillField(nickname, "nickname") ||
+		!_fillField(phone, "phone number") ||
+		!_fillField(secret, "darkest secret"))
 	{
-		std::cout << std::setw(10) << _contacts[i].get_index() << "|"
-				  << std::setw(10) << _formatString(_contacts[i].get_first_name()) << "|"
-				  << std::setw(10) << _formatString(_contacts[i].get_last_name()) << "|"
-				  << std::setw(10) << _formatString(_contacts[i].get_nickname())
-				  << std::endl;
-	}
-}
-
-void PhoneBook::_displayContact(Contact contact)
-{
-	std::cout << "=== VIEW CONTACT ===" << std::endl;
-	std::cout << "First name: " << contact.get_first_name() << std::endl
-			  << "Last name: " << contact.get_last_name() << std::endl
-			  << "Nickname: " << contact.get_nickname() << std::endl
-			  << "Phone number: " << contact.get_phone() << std::endl
-			  << "Darkest Secret: " << contact.get_secret() << std::endl;
-	std::cout << "=================" << std::endl
-			  << std::endl;
-}
-
-void PhoneBook::addContact()
-{
-
-	int index = _count % 8;
-	Contact contact = _collectContactData(index);
-	if (contact.get_index() == -1)
-		return;
-	if (_count >= 8)
-	{
-		std::cout << "Replacing contact at position " << index << std::endl;
+		return Contact();
 	}
 
-	this->_contacts[index] = contact;
-	_count++;
-	std::cout << "Contact " << contact.get_first_name() << " was created" << std::endl;
+	Contact contact =
+		Contact(index, first_name, last_name, nickname, phone, secret);
+	std::cout << "Contact successfully added!" << std::endl << std::endl;
+
+	return contact;
 }
 
-bool PhoneBook::_validateString(std::string field)
-{
-	if (field.empty() || field.find_first_not_of(" \t\n\r") == std::string::npos)
-	{
-		std::cout << "This field cannot be empty. Please try again" << std::endl;
-		return false;
-	}
-	return true;
-}
-
-bool PhoneBook::_validateNumber(std::string field)
-{
-	if (field.empty() || field.find_first_not_of(" \t\n\r") == std::string::npos)
-	{
-		std::cout << "This field cannot be empty. Please try again" << std::endl;
-		return false;
-	}
-	if (field.find_first_not_of("0123456789") != std::string::npos)
-	{
-		std::cout << "Please enter a valid number (only digits)" << std::endl;
-		return false;
-	}
-	return true;
-}
-
-bool PhoneBook::_fillField(std::string& field, std::string field_name)
+bool PhoneBook::_fillField(std::string &field, std::string field_name)
 {
 	std::cout << "Please enter the " << field_name << " :" << std::endl;
 	if (!std::getline(std::cin, field))
@@ -147,32 +116,67 @@ bool PhoneBook::_fillField(std::string& field, std::string field_name)
 	}
 	return true;
 }
-
-Contact PhoneBook::_collectContactData(int index)
+void PhoneBook::_displayContactTable(int contacts_to_show)
 {
-	std::string first_name;
-	std::string last_name;
-	std::string nickname;
-	std::string phone;
-	std::string secret;
+	std::cout << "Which contact would you like to see?" << std::endl;
+	std::cout << std::setw(10) << "Index" << "|" << std::setw(10)
+			  << "First Name" << "|" << std::setw(10) << "Last Name" << "|"
+			  << std::setw(10) << "Nickname" << std::endl;
+	std::cout << "__________|__________|__________|__________" << std::endl;
 
-	std::cout << "=== ADD NEW CONTACT ===" << std::endl;
-	if (!_fillField(first_name, "first name") ||
-		!_fillField(last_name, "last name") ||
-		!_fillField(nickname, "nickname") ||
-		!_fillField(phone, "phone number") ||
-		!_fillField(secret, "darkest secret")
-	)
+	for (int i = 0; i < contacts_to_show; i++)
 	{
-		return Contact();
+		std::cout << std::setw(10) << _contacts[i].get_index() << "|"
+				  << std::setw(10)
+				  << _formatString(_contacts[i].get_first_name()) << "|"
+				  << std::setw(10)
+				  << _formatString(_contacts[i].get_last_name()) << "|"
+				  << std::setw(10) << _formatString(_contacts[i].get_nickname())
+				  << std::endl;
 	}
-
-	Contact contact = Contact(index, first_name, last_name, nickname, phone, secret);
-	std::cout << "Contact successfully added!" << std::endl
-			  << std::endl;
-
-	return contact;
 }
+
+void PhoneBook::_displayContact(Contact contact)
+{
+	std::cout << "=== VIEW CONTACT ===" << std::endl;
+	std::cout << "First name: " << contact.get_first_name() << std::endl
+			  << "Last name: " << contact.get_last_name() << std::endl
+			  << "Nickname: " << contact.get_nickname() << std::endl
+			  << "Phone number: " << contact.get_phone() << std::endl
+			  << "Darkest Secret: " << contact.get_secret() << std::endl;
+	std::cout << "=================" << std::endl << std::endl;
+}
+
+
+bool PhoneBook::_validateString(std::string field)
+{
+	if (field.empty() ||
+		field.find_first_not_of(" \t\n\r") == std::string::npos)
+	{
+		std::cout << "This field cannot be empty. Please try again"
+				  << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool PhoneBook::_validateNumber(std::string field)
+{
+	if (field.empty() ||
+		field.find_first_not_of(" \t\n\r") == std::string::npos)
+	{
+		std::cout << "This field cannot be empty. Please try again"
+				  << std::endl;
+		return false;
+	}
+	if (field.find_first_not_of("0123456789") != std::string::npos)
+	{
+		std::cout << "Please enter a valid number (only digits)" << std::endl;
+		return false;
+	}
+	return true;
+}
+
 
 Contact PhoneBook::_getContact(int index) const
 {
